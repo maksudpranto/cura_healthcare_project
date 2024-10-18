3fcdc490f501f36590471d4d7c913310ac1e52f4
maksudpranto <maksudpranto@gmail.com>
2024-10-18 09:09:38 +0600

    Added Homepage Title Verification during Login

--- TestCases/test_validLogin.py
+++ TestCases/test_validLogin.py
@@ -1,10 +1,14 @@
 from playwright.sync_api import Page,expect
 from Pages.LoginPage import LoginPage
+from Pages.Homepage import Homepage
 
 
 def testValidLogin(setup)->None:
     page = setup
     login_credentials = {"Username":"John Doe","Password":"ThisIsNotAPassword"}
+
+    homepageVerifcation = Homepage(page)
+    homepageVerifcation.verifyHomepageTitle("CURA Healthcare Service")
     loginPage = LoginPage(page)
     login_completion = loginPage.performLogin(login_credentials)
 
